9531fc6bec430ef5a4800acb26cae48ffff4761b
Krystian Maksymowicz <krystian.maksymowicz@polpharmabiologics.com>
2024-12-16 18:57:07 +0100

    Increased route metric for disabling malicious route from 60 to 1024

--- Public/Disable-PanGPALanRestriction.ps1
+++ Public/Disable-PanGPALanRestriction.ps1
@@ -12,12 +12,12 @@ function Disable-PanGPALanRestriction {
         $_.Count -eq 2 -and
         $_.Name -ne "0.0.0.0/0"
     } |
-    Select-Object -ExpandProperty Group |
+    Select-Object -ExpandProperty Group | 
     Where-Object -FilterScript { $_.ifIndex -eq $PanGPInterface.InterfaceIndex }
 
     foreach ($Route in $MaliciousRoutes) {
         for ($i = 0; $i -lt 5; $i++) {
-            $Route | Set-NetRoute -RouteMetric 60
+            $Route | Set-NetRoute -RouteMetric 1024
         }
     }
 }
